43ddab0a11a41e38ffebe84528da1ce18edcdf55
B123code <90937929+B123code@users.noreply.github.com>
2022-09-15 19:09:14 -0500

    3rd update

--- main.js
+++ main.js
@@ -12,5 +12,24 @@ function take() {
     Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML='<img id="capture" src="'+data_uri+'"/>'
     });
+    img=document.getElementById("capture");
+    classifier.classify(img, gotresult);
 }
-console.log("ML 5 version: ", ml5.version)
+console.log("ML 5 version: ", ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/KBd9JSd05/model.json", modelloaded);
+
+function modelloaded() {
+    console.log("model loaded");
+}
+
+function gotresult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result-obj").innerHTML=results[0].label;
+        document.getElementById("result-acc").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
